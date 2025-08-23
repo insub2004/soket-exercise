db98a27c38c7afa8b0367d6fd10c446471df7142
insub2004 <insub2004@naver.com>
2025-08-23 12:41:17 +0900

    step9

--- src/main/java/com/nhnacademy/server/method/response/ResponseFactory.java
+++ src/main/java/com/nhnacademy/server/method/response/ResponseFactory.java
@@ -14,6 +14,8 @@ package com.nhnacademy.server.method.response;
 
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,16 +40,18 @@ public class ResponseFactory {
     
     static {
         //TODO#2-1 "com.nhnacademy.server" 페키지를 기준으로 class를 scan 합니다.
-        Reflections reflections = new Reflections("do something");
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("com.nhnacademy.server")));
         //TODO#2-2 reflections로 부터 Response.class를 구현한 subType을 조회 합니다.
-        Set<Class<? extends Response>> classes = null;
+        Set<Class<? extends Response>> classes = reflections.getSubTypesOf(Response.class);
 
         for (Class<? extends Response> clazz : classes) {
             try {
                 //TODO#2-3 getDeclaredConstructor().newInstance() method를 호출해서 인스턴스를 생성 합니다.
-                Response response = null;
+                Response response = clazz.getDeclaredConstructor().newInstance();
                 log.debug("response-factory init :  instance :{}", response.getClass().getName() );
                 //TODO#2-4 생성된 response instance를 responseList에 추가 합니다.
+                responseList.add(response);
 
             } catch (Exception e) {
                 e.printStackTrace();
