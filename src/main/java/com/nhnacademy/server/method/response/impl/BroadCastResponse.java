f4abeca41149dd419e8be84b922eeebc4a64ff69
insub2004 <insub2004@naver.com>
2025-08-23 10:45:55 +0900

    step8

--- src/main/java/com/nhnacademy/server/method/response/impl/BroadCastResponse.java
+++ src/main/java/com/nhnacademy/server/method/response/impl/BroadCastResponse.java
@@ -27,7 +27,7 @@ public class BroadCastResponse implements Response {
     @Override
     public String getMethod() {
         //TODO#1-1 method = "broadcast" 설정 합니다.
-        return "";
+        return "broadcast";
     }
 
     @Override
@@ -39,10 +39,27 @@ public class BroadCastResponse implements Response {
             - value 값은 client에게 전송할 message 입니다.
          */
 
-        List<String> ids = null;
+        List<String> ids = MessageServer.getClientIds();
         int sendCount=0;
 
-        
+        for (String id : ids) {
+            Socket clientSocket = MessageServer.getClientSocket(id);
+            if (clientSocket.isClosed()) {
+                if (Session.isLogin()) {
+                    MessageServer.removeClient(Session.getCurrentId());
+                }
+                continue;
+            }
+            try {
+                log.debug("id:{},{}", id, clientSocket);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), false);
+                out.println(value);
+                out.flush();
+                sendCount++;
+            } catch (IOException e) {
+                log.debug("broadcast-error:{}",e.getMessage(),e);
+            }
+        }
 
         return String.format("{%d}명에게 \"{%S}\"를 전송 했습니다.",sendCount,value);
     }
